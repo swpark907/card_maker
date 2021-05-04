38fb8c2db3b550005547707d1a7cf9493907e963
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-05-04 18:57:29 +0900

    Add loading spinner for file input

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,7 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './image_file_input.module.css'
 
 const ImageFileInput = ({imageUploader, name, onFileChange}) => {
+
+    const [loading, setLoading] = useState(false);    
+
     const inputRef = useRef()
     const onButtonClick = (event) => {
         event.preventDefault();
@@ -9,6 +12,7 @@ const ImageFileInput = ({imageUploader, name, onFileChange}) => {
     }
 
     const onChange = async event => {
+        setLoading(true);
         console.log(event.target.files[0])
         const uploaded = await imageUploader.upload(event.target.files[0])
         console.log(uploaded);
@@ -16,14 +20,17 @@ const ImageFileInput = ({imageUploader, name, onFileChange}) => {
             name: uploaded.original_filename,
             url: uploaded.url,
         })
+        setLoading(false);
     }
 
     return(
         <div className={styles.container}>
             <input className = {styles.input} ref={inputRef}  type="file" accept="image/*" name="file" onChange={onChange}/>
-            <button className={styles.button} onClick={onButtonClick}>
+            {!loading && <button className={`${styles.button} ${name ? styles.pink : styles.grey }`} onClick={onButtonClick}>
                 {name || "No file"}
-            </button>
+            </button>}
+
+            {loading && <div className={styles.loading}></div>}
 
         </div>
     )
