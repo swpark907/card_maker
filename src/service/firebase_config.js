183c00154fe8612262c240f17da48fea50b519c2
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-01-20 18:59:22 +0900

    realtime data set to firebase

--- src/service/firebase_config.js
+++ src/service/firebase_config.js
@@ -4,8 +4,8 @@ import firebase from 'firebase';
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_DB_URL,
-    databaseURL: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
     };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
